1a83bb7f47c692f7a8405e25d8aa54f432f9e1d7
Md. Shahidul Islam <shanto57575@gmail.com>
2025-06-22 23:25:27 +0600

    all done

--- src/server.ts
+++ src/server.ts
@@ -9,18 +9,24 @@ const PORT = process.env.PORT || 3001;
 const connectDB = async () => {
   try {
     const dbConnection = await mongoose.connect(
-      process.env.MONGODB_URL as string
+      process.env.MONGODB_URL as string,
+      {
+        serverSelectionTimeoutMS: 30000,
+        connectTimeoutMS: 30000,
+      }
     );
+
     console.log(
       `MONGODB CONNECTED!!! CONNECTION HOST: ${dbConnection.connection.host}`
     );
+
+    app.listen(PORT, () => {
+      console.log(`SERVER IS RUNNING ON PORT : ${PORT}`);
+    });
   } catch (error) {
-    console.log("Database connection error", error);
+    console.error("❌ Database connection error", error);
+    process.exit(1);
   }
 };
 
 connectDB();
-
-app.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING ON PORT : ${PORT}`);
-});
